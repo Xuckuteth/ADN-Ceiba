3715b9cfea9dbc5ed1531663022cb2cad1bdef33
estiven.montoya <estiven.montoya@ceiba.com.co>
2022-04-18 12:45:19 -0500

    Code Smells 13

--- microservicio/dominio/src/test/java/com/ceiba/alquiler/entidad/AlquilerTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/alquiler/entidad/AlquilerTest.java
@@ -3,6 +3,8 @@ package com.ceiba.alquiler.entidad;
 import com.ceiba.BasePrueba;
 import com.ceiba.alquiler.modelo.entidad.Alquiler;
 import com.ceiba.alquiler.servicio.testdatabuilder.AlquilerTestDataBuilder;
+import com.ceiba.cliente.modelo.entidad.Cliente;
+import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,12 +15,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AlquilerTest {
     @Test
-    @DisplayName("Deberia crear correctamente el alquiler")
-    void deberiaCrearCorrectamenteElAlquiler() {
+    @DisplayName("Deberia crear correctamente el alquiler si el cliente es de estado Estandar")
+    void deberiaCrearCorrectamenteElAlquilerConEstadoEstandar() {
         // arrange
         LocalDate fechaAlquiler = LocalDate.now();
         //act
-        Alquiler alquiler= new AlquilerTestDataBuilder().conId(1L).build();
+        Alquiler alquiler= new AlquilerTestDataBuilder().conId(1L).conCliente(new Cliente(1L, "Estiven", "Estandar")).build();
+        //assert
+        assertEquals(1, alquiler.getId());
+
+        assertEquals(1, alquiler.getCliente().getId());
+        assertEquals("Estiven", alquiler.getCliente().getNombre());
+        assertEquals("Estandar", alquiler.getCliente().getEstado());
+
+        assertEquals(1, alquiler.getPelicula().getId());
+        assertEquals("StarWars", alquiler.getPelicula().getNombre());
+        assertEquals("DVD", alquiler.getPelicula().getFormato());
+
+        assertEquals(fechaAlquiler, alquiler.getFechaAlquiler());
+        assertEquals(alquiler.getFechaDevolucion(), alquiler.getFechaDevolucion());
+        assertEquals("8.0" + " USD", alquiler.getValor());
+    }
+
+    @Test
+    @DisplayName("Deberia crear correctamente el alquiler si el cliente es de estado Incumplimiento")
+    void deberiaCrearCorrectamenteElAlquilerConEstadoIncumplimiento() {
+        // arrange
+        LocalDate fechaAlquiler = LocalDate.now();
+        //act
+        Alquiler alquiler= new AlquilerTestDataBuilder().conId(1L).conCliente(new Cliente(1L, "Estiven", "Incumplimiento")).build();
         //assert
         assertEquals(1, alquiler.getId());
 
@@ -33,6 +58,41 @@ public class AlquilerTest {
         assertEquals(fechaAlquiler, alquiler.getFechaAlquiler());
         assertEquals(alquiler.getFechaDevolucion(), alquiler.getFechaDevolucion());
         assertEquals("9.6" + " USD", alquiler.getValor());
+    }
+
+    @Test
+    @DisplayName("Deberia crear correctamente el alquiler si el cliente es de estado Incumplimiento X2")
+    void deberiaCrearCorrectamenteElAlquilerConEstadoIncumplimientoX2() {
+        // arrange
+        LocalDate fechaAlquiler = LocalDate.now();
+        //act
+        Alquiler alquiler= new AlquilerTestDataBuilder().conId(1L).conCliente(new Cliente(1L, "Estiven", "Incumplimiento X2")).build();
+        //assert
+        assertEquals(1, alquiler.getId());
+
+        assertEquals(1, alquiler.getCliente().getId());
+        assertEquals("Estiven", alquiler.getCliente().getNombre());
+        assertEquals("Incumplimiento X2", alquiler.getCliente().getEstado());
+
+        assertEquals(1, alquiler.getPelicula().getId());
+        assertEquals("StarWars", alquiler.getPelicula().getNombre());
+        assertEquals("DVD", alquiler.getPelicula().getFormato());
+
+        assertEquals(fechaAlquiler, alquiler.getFechaAlquiler());
+        assertEquals(alquiler.getFechaDevolucion(), alquiler.getFechaDevolucion());
+        assertEquals("12.0" + " USD", alquiler.getValor());
+    }
+
+    @Test
+    void deberiaFallarSiElClienteEstaEnEstadoVetado() {
+
+        //Arrange
+        AlquilerTestDataBuilder alquilerTestDataBuilder = new AlquilerTestDataBuilder().conCliente(new Cliente(1L, "Estiven", "Vetado")).conId(1L);
+        //act-assert
+        BasePrueba.assertThrows(() -> {
+                    alquilerTestDataBuilder.build();
+                },
+                ExcepcionValorInvalido.class, "El cliente se encuentra vetado y por lo tanto no puede crear un alquiler");
     }
 
     @Test
